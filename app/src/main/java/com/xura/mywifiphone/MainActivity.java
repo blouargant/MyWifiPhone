cb863a6ca40140bc266ee489cc1ceced661c1d9d
bertrand <bertrand.louargant@atoutlinux.net>
2016-02-08 20:10:36 +0100

    Add security settings

--- app/src/main/java/com/xura/mywifiphone/MainActivity.java
+++ app/src/main/java/com/xura/mywifiphone/MainActivity.java
@@ -1,12 +1,17 @@
 package com.xura.mywifiphone;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
@@ -20,6 +25,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.security.acl.Permission;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,6 +34,11 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar appToolbar;
     private DrawerLayout mDrawerLayout;
     private TabLayout tabLayout;
+
+    private static final int REQUEST_CONTACTS = 0;
+    private static String[] PERMISSIONS_CONTACT = {Manifest.permission.READ_CONTACTS,
+            Manifest.permission.WRITE_CONTACTS};
+    private View mLayout;
 
     private int[] tabIconsSelected = {
         R.drawable.ic_star_white_24dp,
@@ -40,12 +51,63 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.ic_contact_phone_grey_24dp
     };
 
+    private void checkPermissionsBeforeSetup() {
+
+        String contactPerm = Manifest.permission.READ_CONTACTS;
+        int granted = PackageManager.PERMISSION_GRANTED;
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, contactPerm) != granted) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, contactPerm)) {
+
+                //Explain to the user why we need to read the contacts
+                Snackbar.make(mLayout, R.string.permission_contacts_rationale,
+                        Snackbar.LENGTH_INDEFINITE)
+                        .setAction("OK", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        PERMISSIONS_CONTACT, REQUEST_CONTACTS);
+                            }
+                        })
+                        .show();
+
+            } else {
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            PERMISSIONS_CONTACT, REQUEST_CONTACTS);
+            }
+        } else {
+            setupActivity();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    setupActivity();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkPermissionsBeforeSetup();
+    }
 
+    private void setupActivity() {
         appToolbar = (Toolbar) findViewById(R.id.top_app_bar);
         setSupportActionBar(appToolbar);
 
@@ -53,12 +115,6 @@ public class MainActivity extends AppCompatActivity {
         ab.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
         ab.setDisplayHomeAsUpEnabled(true);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-        /*
-        NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment)
-                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
-        drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout)findViewById(R.id.drawer_layout), appToolbar);
-        */
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         if (navigationView != null) {
@@ -104,10 +160,9 @@ public class MainActivity extends AppCompatActivity {
                 tab.setIcon(tabIconsSelected[pos]);
             }
         });
-
     }
 
-    @Override
+        @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
