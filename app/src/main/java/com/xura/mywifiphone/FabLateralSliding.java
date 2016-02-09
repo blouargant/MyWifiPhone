df095ad3eef5bd522a30bb58319364480ac27190
bertrand <bertrand.louargant@atoutlinux.net>
2016-02-09 18:17:10 +0100

    start fab sliding

---
+++ app/src/main/java/com/xura/mywifiphone/FabLateralSliding.java
@@ -0,0 +1,41 @@
+package com.xura.mywifiphone;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by bertrand on 09/02/16.
+ */
+public class FabLateralSliding {
+    public FabLateralSliding(Context context, AttributeSet attrs) {
+        super();
+    }
+
+    @Override
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout,
+                               FloatingActionButton child,
+                               View target,
+                               int dxConsumed,
+                               int dyConsumed,
+                               int dxUnconsumed,
+                               int dyUnconsumed) {
+
+        //super();
+
+        //child -> Floating Action Button
+        if (child.getVisibility() == View.VISIBLE && dyConsumed > 0) {
+            child.hide();
+        } else if (child.getVisibility() == View.GONE && dyConsumed < 0) {
+            child.show();
+        }
+    }
+
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL;
+    }
+}
